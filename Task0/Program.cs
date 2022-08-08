ec3216b696a034972425181f2e0b234767f01f93
Evgeniya <sluchevskaya0309@yandex.ru>
2022-08-08 10:19:30 +0300

    Добавила изменения в код

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -4,8 +4,16 @@
 // -3 -> 9
 // -7 -> 49
 
-Console.Write("Введите целое число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int square = number * number;
-Console.Write($"Квадрат числа {number} = {square}");
+// Console.Write("Введите целое число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int square = number * number;
+// Console.Write($"Квадрат числа {number} = {square}");
 
+
+Console.Write("Введите целое число1: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число2: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
+Console.WriteLine($"Квадрат числа {number1} = {square}");
