2f6ef23edcc659d75e028b60a6b265aae1c3f40f
Evgeniya <sluchevskaya0309@yandex.ru>
2022-08-11 13:12:48 +0300

    решили 4 и 5 задачу

---
+++ семинар2_функции/зад4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет,
+// кратно ли оно одномременно 7 и 23.
+// 14 -> no
+// 46-> no
+// 161 -> yes
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Endnum(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+
+bool result = Endnum(number);
+string res = result ? "true" : "false";
+Console.Write(res);
---
+++ семинар2_функции/зад5/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает 2 числа и проверяет,
+// является ли одно число квадратом другого
+// 5,25 -> yes
+// -4,16 -> yes
+// 8,9 -> no
+
+Console.Write("Введите два числа. Первое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool Endnum(int num,int num2 )
+{
+    return num * num == num2 || num2 * num2 == num;
+
+}
+bool result = Endnum(number,number2 );
+string res = result ? "true" : "false";
+Console.Write(res);
