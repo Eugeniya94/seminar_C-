b0b61311ed210c44c86a2578bd601081ff70841a
Evgeniya <sluchevskaya0309@yandex.ru>
2022-08-14 13:48:59 +0300

    решили 2 задачу

---
+++ семинар 3_функции/ex02/Program.cs
@@ -0,0 +1,17 @@
+﻿// напишите программу, которая по заданному номеру четверти
+//  показывает диапазон возможных координат точек в этой четверти (x,y)
+
+Console.Write("Введите номер четверти: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string Range (int num)
+{
+    if (num == 1) return "Значение переменных: x > 0 && y > 0";
+    if (num == 2) return "Значение переменных: x < 0 && y > 0";
+    if (num == 3) return "Значение переменных: x < 0 && y < 0";
+    if (num == 4) return "Значение переменных: x > 0 && y < 0";
+  return "Введены некорректные координаты";
+ 
+}
+ string result = Range(number);
+ Console.WriteLine(result);
