766835d2779561b6d6601783c2cee04d6ee13690
Evgeniya <sluchevskaya0309@yandex.ru>
2022-08-16 10:02:56 +0300

    Решили задачу ex04

---
+++ семинар 3_функции/ex04/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N) и выдает таблицу квадратов чисел от 1 до N.
+// 5-> 1,4,9,16,25.
+// 2-> 1,4.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Таблица квадратов от 1 до {number}: ");
+int count = 1;
+while (count <= number)
+{
+    Console.WriteLine($"{count}-> {Math.Pow(count, 2)}");
+    count++;
+}
