e03e6b477742043b2c9128d67ee72338f70d0585
Evgeniya <sluchevskaya0309@yandex.ru>
2022-08-17 18:47:59 +0300

    решили все задачи

---
+++ семинар_4/ex2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу,которая принимает на вход число и выдает количество цифр в числе.
+// 456->3
+// 78->2
+// 89126->5
+
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int CountNum(int num)
+{
+    int count = 1;
+    while (num >= 10)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+int res = CountNum(number);
+Console.WriteLine($"Количество цифр в числе равно {res}");
+
+// for (; number >=10;number = number/10)
+// {
+//    count++; 
+// }
+// Console.WriteLine($"Количество цифр в числе равно {count}");
---
+++ семинар_4/ex3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает число N и выдает произведение всех чисел от 1 до N.
+// 4->24
+// 5->120
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+//int mult = 1;
+
+// for (int i = 1; i <= number; i++)
+// {
+//     mult *= i;
+// }
+//Console.WriteLine($"Произведение чисел от 1 до {number} равна {mult}");
+
+int MultNum(int num)
+{
+ int mult = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        mult *= i;
+    }
+    return mult;
+}
+int res = MultNum(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} равна {res}");
